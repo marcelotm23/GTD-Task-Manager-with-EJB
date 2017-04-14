cc92437a710ea11fd077d0a1a39cfd3a826b5c69
marcelotm23 <marcelo.torrejn@gmail.com>
2017-04-14 21:17:42 +0200

    Eliminar usuario y otras mejoras
    -Falta completar listar usuarios con todos los datos que se pide
    mostrar, que son nuevos como nºtareas...
    -Falta gestionar adecuadamente las excepciones ya que las
    BusinessException dan lugar a EJBException

--- sdi3-28.Cli-EJB/src/com/sdi/client/ui/action/DeshabilitarUsuarioAction.java
+++ sdi3-28.Cli-EJB/src/com/sdi/client/ui/action/DeshabilitarUsuarioAction.java
@@ -7,23 +7,29 @@ import alb.util.console.Console;
 import alb.util.menu.Action;
 
 import com.sdi.business.AdminService;
+import com.sdi.business.exception.BusinessException;
 
-public class DeshabilitarUsuarioAction implements Action{
+public class DeshabilitarUsuarioAction implements Action {
 	private static final String BASE_JNDI_KEY = "sdi3-28/" + "sdi3-28.EJB/";
 	private static final String ADMIN_SERVICE_JNDI_KEY = BASE_JNDI_KEY
 			+ "EjbAdminService!"
 			+ "com.sdi.business.impl.admin.RemoteAdminService";
+
 	@Override
 	public void execute() throws Exception {
 		Long id = Console.readLong("Id del usuario");
 		Context ctx = new InitialContext();
 		AdminService adminService = (AdminService) ctx
 				.lookup(ADMIN_SERVICE_JNDI_KEY);
+		try {
+			adminService.disableUser(id);
+			Console.println("Usuario deshabilitado");
+		} catch (BusinessException e) {
+			Console.println("ERROR:" + e.getMessage());
+		}
+
 		
-		adminService.disableUser(id);
-		
-		Console.println("Usuario deshabilitado");
-		
+
 	}
 
 }
--- sdi3-28.Cli-EJB/src/com/sdi/client/ui/action/EliminarRastroUsuarioAction.java
+++ sdi3-28.Cli-EJB/src/com/sdi/client/ui/action/EliminarRastroUsuarioAction.java
@@ -1,13 +1,34 @@
 package com.sdi.client.ui.action;
 
+import javax.naming.Context;
+import javax.naming.InitialContext;
+
+import com.sdi.business.AdminService;
+import com.sdi.business.exception.BusinessException;
+
+import alb.util.console.Console;
 import alb.util.menu.Action;
 
 public class EliminarRastroUsuarioAction implements Action{
 
+	private static final String BASE_JNDI_KEY = "sdi3-28/" + "sdi3-28.EJB/";
+	private static final String ADMIN_SERVICE_JNDI_KEY = BASE_JNDI_KEY
+			+ "EjbAdminService!"
+			+ "com.sdi.business.impl.admin.RemoteAdminService";
 	@Override
 	public void execute() throws Exception {
-		// TODO Auto-generated method stub
+		Long id = Console.readLong("Id del usuario");
+		Context ctx = new InitialContext();
+		AdminService adminService = (AdminService) ctx
+				.lookup(ADMIN_SERVICE_JNDI_KEY);
+		try{
+			adminService.deepDeleteUser(id);
+		} catch (BusinessException e) {
+			Console.println("ERROR:" + e.getMessage());
+		}
 		
+		
+		Console.println("Usuario eliminado");
 	}
 
 }
