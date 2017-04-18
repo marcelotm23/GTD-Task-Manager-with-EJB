0475a167144a6c2527b9d6194ae9de2ce327b3a9
marcelotm23 <marcelo.torrejn@gmail.com>
2017-04-18 11:37:14 +0200

    Cliente SOAP
    De momento en el servidor interno

---
+++ sdi3-28.Cli-SOAP/src/com/sdi/client/ui/action/DeshabilitarUsuarioAction.java
@@ -0,0 +1,26 @@
+package com.sdi.client.ui.action;
+
+import alb.util.console.Console;
+import alb.util.menu.Action;
+
+import com.sdi.ws.AdminService;
+import com.sdi.ws.EjbAdminServiceService;
+
+public class DeshabilitarUsuarioAction implements Action {
+
+	@Override
+	public void execute() throws Exception {
+		Long id = Console.readLong("Id del usuario");
+		
+		AdminService adminService = new EjbAdminServiceService()
+									.getAdminServicePort();
+		try {
+			adminService.disableUser(id);
+			Console.println("Usuario deshabilitado");
+		} catch (Exception e) {
+			Console.println("ERROR:" + e.getMessage());
+		}
+
+	}
+
+}
